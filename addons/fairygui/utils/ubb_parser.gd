2115ce2bc3cc77098cbef15e9d760ee0b5233c04
citizenl <hi@citizenl.me>
2026-07-11 01:56:13 +0800

    feat: expand UBB rich text parsing

--- addons/fairygui/utils/ubb_parser.gd
+++ addons/fairygui/utils/ubb_parser.gd
@@ -1,21 +1,184 @@
 class_name FGUIUBBParser
 extends RefCounted
 
+## Converts FairyGUI UBB into the BBCode accepted by Godot RichTextLabel.
+##
+## Image tags deliberately remain [img]ui://...[/img]. FGUIRichTextField
+## resolves those package URLs before forwarding the surrounding BBCode to Godot.
+
 static var default_parser := FGUIUBBParser.new()
 
 var default_img_width: int = 0
 var default_img_height: int = 0
+var last_color: String = ""
+var last_size: String = ""
+var link_underline: bool = false
+var link_color: String = ""
+
+var _text: String = ""
+var _read_pos: int = 0
+var _align_stack: Array[String] = []
 
 
 func parse(text: String, remove: bool = false) -> String:
-	if remove:
-		return _strip_tags(text)
-	var result := text
-	result = result.replace("[size=", "[font_size=").replace("[/size]", "[/font_size]")
+	_text = text
+	_read_pos = 0
+	_align_stack.clear()
+	last_color = ""
+	last_size = ""
+
+	var result := ""
+	var pos := 0
+	while true:
+		var tag_start := _text.find("[", pos)
+		if tag_start == -1:
+			break
+		if tag_start > 0 and _text[tag_start - 1] == "\\":
+			result += _text.substr(pos, tag_start - pos - 1)
+			result += "["
+			pos = tag_start + 1
+			continue
+
+		result += _text.substr(pos, tag_start - pos)
+		var tag_end := _text.find("]", tag_start + 1)
+		if tag_end == -1:
+			pos = tag_start
+			break
+
+		var end := tag_start + 1 < _text.length() and _text[tag_start + 1] == "/"
+		var content_start := tag_start + (2 if end else 1)
+		var raw_tag := _text.substr(content_start, tag_end - content_start)
+		_read_pos = tag_end + 1
+
+		var attribute := ""
+		var separator := raw_tag.find("=")
+		if separator != -1:
+			attribute = raw_tag.substr(separator + 1)
+			raw_tag = raw_tag.substr(0, separator)
+		var tag_name := raw_tag.to_lower()
+
+		if _is_supported_tag(tag_name):
+			if not remove:
+				var replacement := _convert_tag(tag_name, end, attribute)
+				if replacement != null:
+					result += str(replacement)
+		else:
+			result += _text.substr(tag_start, _read_pos - tag_start)
+		pos = _read_pos
+
+	if pos < _text.length():
+		result += _text.substr(pos)
+	_text = ""
 	return result
 
 
-func _strip_tags(text: String) -> String:
-	var regex := RegEx.new()
-	regex.compile("\\[[^\\]]+\\]")
-	return regex.sub(text, "", true)
+func _is_supported_tag(tag_name: String) -> bool:
+	match tag_name:
+		"url", "img", "b", "i", "u", "sup", "sub", "strike", "color", "font", "size", "align", "br":
+			return true
+		_:
+			return false
+
+
+func _convert_tag(tag_name: String, end: bool, attribute: String) -> Variant:
+	match tag_name:
+		"url":
+			if end:
+				var close := ""
+				if link_color != "":
+					close += "[/color]"
+				if link_underline:
+					close += "[/u]"
+				return close + "[/url]"
+			var href := attribute
+			if href == "":
+				var text_href := _get_tag_text()
+				href = str(text_href) if text_href != null else ""
+			var open := "[url=%s]" % href
+			if link_underline:
+				open += "[u]"
+			if link_color != "":
+				open += "[color=%s]" % link_color
+			return open
+		"img":
+			if end:
+				return null
+			var source := attribute
+			if source == "":
+				var image_source := _consume_image_source()
+				if image_source == null:
+					return null
+				source = str(image_source)
+			return "[img]%s[/img]" % source if source != "" else null
+		"b", "i", "u", "sup", "sub":
+			return "[/%s]" % tag_name if end else "[%s]" % tag_name
+		"strike":
+			return "[/s]" if end else "[s]"
+		"color":
+			if end:
+				return "[/color]"
+			last_color = attribute
+			return "[color=%s]" % attribute
+		"size":
+			if end:
+				return "[/font_size]"
+			last_size = attribute
+			return "[font_size=%s]" % attribute
+		"font":
+			return "[/font]" if end else "[font=%s]" % attribute
+		"align":
+			if end:
+				if _align_stack.is_empty():
+					return null
+				return "[/%s]" % _align_stack.pop_back()
+			var alignment := attribute.to_lower()
+			if not ["left", "center", "right", "fill"].has(alignment):
+				return null
+			_align_stack.append(alignment)
+			return "[%s]" % alignment
+		"br":
+			return "" if end else "\n"
+		_:
+			return null
+
+
+func _get_tag_text() -> Variant:
+	var pos := _read_pos
+	var result := ""
+	while true:
+		var next_tag := _text.find("[", pos)
+		if next_tag == -1:
+			return null
+		if next_tag > 0 and _text[next_tag - 1] == "\\":
+			result += _text.substr(pos, next_tag - pos - 1)
+			result += "["
+			pos = next_tag + 1
+			continue
+		result += _text.substr(pos, next_tag - pos)
+		return result
+	return null
+
+
+func _consume_image_source() -> Variant:
+	var pos := _read_pos
+	var result := ""
+	while true:
+		var tag_start := _text.find("[", pos)
+		if tag_start == -1:
+			return null
+		if tag_start > 0 and _text[tag_start - 1] == "\\":
+			result += _text.substr(pos, tag_start - pos - 1)
+			result += "["
+			pos = tag_start + 1
+			continue
+
+		var tag_end := _text.find("]", tag_start + 1)
+		if tag_end == -1:
+			return null
+		var tag_name := _text.substr(tag_start + 1, tag_end - tag_start - 1).to_lower()
+		if tag_name != "/img":
+			return null
+		result += _text.substr(pos, tag_start - pos)
+		_read_pos = tag_end + 1
+		return result
+	return null
---
+++ tests/ubb_parser_probe.gd
@@ -0,0 +1,34 @@
+extends SceneTree
+
+
+func _initialize() -> void:
+	var parser := FGUIUBBParser.new()
+	parser.link_underline = true
+	parser.link_color = "#44ccff"
+	var source := "[b]Bold[/b] [size=18]Large[/size] [color=#ff0000]Red[/color] [url=https://example.com]Link[/url] [url]https://plain.example[/url] [img]ui://packageimage[/img] [align=center]Centered[/align] \\[b]"
+	var parsed := parser.parse(source)
+	var expected := "[b]Bold[/b] [font_size=18]Large[/font_size] [color=#ff0000]Red[/color] [url=https://example.com][u][color=#44ccff]Link[/color][/u][/url] [url=https://plain.example][u][color=#44ccff]https://plain.example[/color][/u][/url] [img]ui://packageimage[/img] [center]Centered[/center] [b]"
+	if parsed != expected:
+		_fail("UBB parsing did not map FairyGUI tags to Godot BBCode: %s" % parsed)
+		return
+	if parser.last_color != "#ff0000" or parser.last_size != "18":
+		_fail("UBB parsing did not retain the latest color and size metadata.")
+		return
+	var stripped := parser.parse(source, true)
+	if stripped != "Bold Large Red Link https://plain.example ui://packageimage Centered [b]":
+		_fail("UBB remove mode did not preserve text while stripping recognized tags: %s" % stripped)
+		return
+	var extended := parser.parse("A[strike]B[/strike][br]C[unknown=x]D[/unknown]")
+	if extended != "A[s]B[/s]\nC[unknown=x]D[/unknown]":
+		_fail("UBB parsing did not handle strike, line break, and unknown tags: %s" % extended)
+		return
+	var extended_stripped := parser.parse("A[strike]B[/strike][br]C[unknown=x]D[/unknown]", true)
+	if extended_stripped != "ABC[unknown=x]D[/unknown]":
+		_fail("UBB remove mode stripped unsupported tags or left supported tags behind: %s" % extended_stripped)
+		return
+	quit(0)
+
+
+func _fail(message: String) -> void:
+	push_error(message)
+	quit(1)
